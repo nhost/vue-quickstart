7d005e31b3d5b5622a47222b028a83113021ebc3
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-05-19 11:56:30 +0200

    initial

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div i-carbon-home text-4xl inline-block />
+    <p>Nhost with Vue</p>
+    <p>
+      <em text-sm op75>Quickstart</em>
+    </p>
+    <div py-4 />
+  </div>
+</template>
